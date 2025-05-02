c04921e26786bcefa729b01eda0af7e79f6bde94
Steve Fox <65349887+LexadoF@users.noreply.github.com>
2025-05-02 01:14:15 -0500

    actualizada configuración para despliegue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'http://lexadof.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Kiwi_site',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Kiwi', // Usually your GitHub org/user name.
   projectName: 'Kiwi Docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
